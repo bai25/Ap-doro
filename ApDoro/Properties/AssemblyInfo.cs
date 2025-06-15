8498aedc8c44b0d210d717fe4bd3cd58379c86f1
bai25 <109252599+bai25@users.noreply.github.com>
2025-06-15 17:13:42 +0800

    Create AssemblyInfo.cs

---
+++ ApDoro/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ApDoro")]
+[assembly: AssemblyDescription("Windows登录界面背景修改工具")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ApDoro")]
+[assembly: AssemblyProduct("ApDoro Background Changer")]
+[assembly: AssemblyCopyright("Copyright © ApDoro 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a5b5c5d5-e5f5-4a5b-9c5d-5e5f5a5b5c5d")]
+
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
